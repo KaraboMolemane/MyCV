5d31c7970b1d424c37c5c3b4be9e10351ebc533f
KaraboMolemane <karabopro10@gmail.com>
2022-09-21 20:04:11 +0200

    initial commit

---
+++ js/sharedcode.js
@@ -0,0 +1,10 @@
+//Below are links considered when brainstorming the design of the website 
+//https://blog.hyperiondev.com/index.php/2018/07/17/a-developer-portfolio-what-it-is-and-how-to-get-one/
+//https://andrewborstein.com/
+
+
+
+$(document).ready(function() {
+    //$(".scrollspy").scrollSpy();
+    //$('body').scrollspy({target: ".navbar"})
+});
